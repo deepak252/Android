3cdb6b7d7962389c2d077e60b6adfe37879ed316
Deepak <trojanhorsedc@gmail.com>
2022-09-10 02:52:15 +0530

    Drawer Init

---
+++ 20_Navigation_Drawer_Demo/app/src/main/java/com/example/a20_navigation_drawer_demo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a20_navigation_drawer_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
