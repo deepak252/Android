7fc4b83035a72616b3615b78743f51ddcf9ae21f
Deepak <trojanhorsedc@gmail.com>
2022-09-19 02:01:37 +0530

    Drawer Implementation

--- 20_Navigation_Drawer_Demo/app/src/main/java/com/example/a20_navigation_drawer_demo/MainActivity.java
+++ 20_Navigation_Drawer_Demo/app/src/main/java/com/example/a20_navigation_drawer_demo/MainActivity.java
@@ -1,14 +1,76 @@
 package com.example.a20_navigation_drawer_demo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    NavigationView navView;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbarMain);
+        navView = findViewById(R.id.navView);
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        //Step 1
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this,drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer
+        );
+        drawerLayout.addDrawerListener( toggle );
+        toggle.syncState();
+//        loadMyFragment(new AFragment());
+        // Drawer can toggle now
+
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.optHome){
+                   loadMyFragment(new AFragment());
+                }else if(id==R.id.optBookmarks){
+                    Toast.makeText(MainActivity.this, "Bookmarks", Toast.LENGTH_SHORT).show();
+                }else if(id==R.id.optProfile){
+                    Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                }else if(id==R.id.optSettings){
+                    Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    private void loadMyFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.container, fragment);
+        ft.commit();
     }
 }
