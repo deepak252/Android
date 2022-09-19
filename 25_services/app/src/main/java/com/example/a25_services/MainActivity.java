2286a2f23048b80fcca0c440b4078ae5bb58d272
Deepak <trojanhorsedc@gmail.com>
2022-09-19 23:46:03 +0530

    Android service class implementation

--- 25_services/app/src/main/java/com/example/a25_services/MainActivity.java
+++ 25_services/app/src/main/java/com/example/a25_services/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.a25_services;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnStartService;
+    Button btnStopService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStartService = findViewById(R.id.btnStartService);
+        btnStopService = findViewById(R.id.btnStopService);
+
+        btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this,MusicService.class));
+            }
+        });
+
+        btnStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MusicService.class));
+            }
+        });
     }
 }
---
+++ 25_services/app/src/main/java/com/example/a25_services/MusicService.java
@@ -0,0 +1,34 @@
+package com.example.a25_services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+
+import androidx.annotation.Nullable;
+
+public class MusicService extends Service {
+    MediaPlayer mediaPlayer;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+        mediaPlayer.start();
+
+//        return super.onStartCommand(intent, flags, startId);
+        return START_NOT_STICKY; //Service will automatically destroy after execution
+    }
+
+    @Override
+    public void onDestroy() {
+        //Stop music on destroy service
+        mediaPlayer.stop();
+        super.onDestroy();
+    }
+}
