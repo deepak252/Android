1494186e2c67e7a40395d4c1a837c20e5ad198e1
Deepak <trojanhorsedc@gmail.com>
2022-09-25 02:22:53 +0530

    Constraint layout starting files

---
+++ 02a_Constraint_Layout_Demo/app/src/main/java/com/example/a02a_constraint_layout_demo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a02a_constraint_layout_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
