34d893eec00cc7ee87b58a0e6565fd4c9f91b2ba
Deepak <trojanhorsedc@gmail.com>
2022-12-16 02:23:19 +0530

    Lifecycle aware components

---
+++ 30_Lifecycle_Aware_Components/app/src/main/java/com/example/a30_lifecycle_aware_components/Observer.java
@@ -0,0 +1,10 @@
+package com.example.a30_lifecycle_aware_components;
+
+import androidx.lifecycle.LifecycleObserver;
+
+public class Observer implements LifecycleObserver {
+
+    public  void onCreate(){
+
+    }
+}
