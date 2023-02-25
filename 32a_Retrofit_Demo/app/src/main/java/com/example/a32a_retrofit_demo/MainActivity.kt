eebe299543607c1f1dbba4757600afa5f24c5973
User <trojanhorsedc@gmail.com>
2023-02-25 12:25:47 +0530

    Fetch data from News API

--- 32a_Retrofit_Demo/app/src/main/java/com/example/a32a_retrofit_demo/MainActivity.kt
+++ 32a_Retrofit_Demo/app/src/main/java/com/example/a32a_retrofit_demo/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.a32a_retrofit_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fetchNews()
+    }
+
+    fun fetchNews(){
+        val news = NewsService.INSTANCE.getNews()
+        news.enqueue(object : Callback<News>{
+            override fun onResponse(call: Call<News>, response: Response<News>) {
+                Log.d("MyTag", response.body().toString())
+                findViewById<TextView>(R.id.tvNews).text = response.body().toString()
+            }
+
+            override fun onFailure(call: Call<News>, t: Throwable) {
+                Log.d("MyTag", "${t.message}")
+            }
+
+        })
     }
 }
---
+++ 32a_Retrofit_Demo/app/src/main/java/com/example/a32a_retrofit_demo/News.kt
@@ -0,0 +1,3 @@
+package com.example.a32a_retrofit_demo
+
+data class News(val totalResults : Int, val articles : List<Article>)
